69fb974620486bf3787b167c7a2e37d184e20faa
Laura Cristina <lauracristina.s@hotmail.com>
2023-10-21 22:37:49 -0300

    log, adicionando outro texto a partir do R.string, mudando tamanho de fonte do texto

--- app/src/main/kotlin/dev/lauracristina/meuprimeiroaplicativo/MainActivity.kt
+++ app/src/main/kotlin/dev/lauracristina/meuprimeiroaplicativo/MainActivity.kt
@@ -2,34 +2,58 @@ package dev.lauracristina.meuprimeiroaplicativo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var primeiraVal: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        primeiraVal = findViewById(R.id.novo_texto)
 
-        val primeiraVal: TextView = findViewById(R.id.novo_texto)
+        val textoOriginal = primeiraVal.text
+        val corOriginal = primeiraVal.textColors
+        val tamanhoOriginal = primeiraVal.textSize
 
-        val textOriginal = primeiraVal.text
-        val textNovo = "ai que delícia ser viado"
+        val corNovo = ContextCompat.getColor(baseContext, R.color.green)
+        val tamanhoNovo =  R.dimen.text_size_novo
+        val textoNovo = getString(R.string.second_label)
 
-        val cor1 = ContextCompat.getColor(baseContext, R.color.green)
-        val cor2 = ContextCompat.getColor(baseContext, R.color.black)
-
-        primeiraVal.setOnClickListener {
-            if (primeiraVal.text.equals(textOriginal)) {
-                primeiraVal.text = textNovo
-                primeiraVal.setTextColor(cor1)
-            } else {
-                primeiraVal.text = textOriginal
-                primeiraVal.setTextColor(cor2)
-
+        with(primeiraVal){
+            setOnClickListener {
+                if (text.equals(textoOriginal)) {
+                    text = textoNovo
+                    setTextColor(corNovo)
+                    setTextSize(TypedValue.COMPLEX_UNIT_PX, baseContext.resources.getDimension(tamanhoNovo))
+                } else {
+                    text = textoOriginal
+                    setTextColor(corOriginal)
+                    setTextSize(TypedValue.COMPLEX_UNIT_PX, tamanhoOriginal)
+                }
             }
-
         }
+    }
 
+    override fun onResume() {
+        super.onResume()
+        primeiraVal.text = "entrou no onResume"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        primeiraVal.text = "entrou no onDestroy"
+        Log.e("MainActivity", "ENTROU NO onDestroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        primeiraVal.text = "entrou no onPause"
+        Log.i("MainActivity", "ENTROU NO ONPAUSE")
     }
 }
 
